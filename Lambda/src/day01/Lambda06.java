4a177e3de719b06b91ab16bcc85714ad1954df09
Murat Kutlu Tuna <muratkutlutuna@gmail.com>
2021-12-29 20:45:18 +0100

    intellijFirst1

---
+++ Lambda/src/day01/Lambda06.java
@@ -0,0 +1,58 @@
+package day01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Lambda06 {
+    public static void main(String[] args) throws IOException {
+        //TASK 01 --> haluk.txt dosyasini okuyunuz.(Console'a yazdiriniz)
+        System.out.println("\n*** haluk.txt dosyasini okuyunuz -->  ");
+        Files.
+                lines(Paths.get("src\\day01\\haluk.txt")).//file e erisip file satirlari akisa alindi
+                forEach(System.out::println);//akistaki elemanlar yazildi
+        System.out.println(Files.lines(Path.of("src/day01/haluk.txt")));//referansini verir
+        //TASK 02 --> haluk.txt dosyasini buyuk harflerle okuyunuz.(Console'a buyuk harflerle yazdiriniz)
+        System.out.println("\n*** haluk.txt dosyasini buyuk harflerle okuyunuz -->  ");
+        Files.
+                lines(Paths.get("src\\day01\\haluk.txt")).//file e erisip file satirlari akisa alindi
+                map(String::toUpperCase).//akisdaki satirlarin byk harf ile update edildi
+                forEach(System.out::println);//akistaki elemanlar yazildi
+        //TASK 03 --> haluk.txt dosyasindaki ilk satiri kucuk harflerle yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki ilk satiri kucuk harflerle okuyunuz 01 -->  ");
+        Files.
+                lines(Paths.get("src\\day01\\haluk.txt")).//file e erisip file satirlari akisa alindi
+                limit(1).//ilk satir alindi
+                map(String::toLowerCase).//harfler kucultuldu
+                forEach(System.out::println);//akistaki elemanlar yazildi
+        //TASK 04 --> haluk.txt dosyasinda "basari" kelimesinin kac satirda gectiginiz yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda basari kelimesinin kac satirda gectigini yazdiriniz -->  ");
+        System.out.println(Files.
+                lines(Paths.get("src\\day01\\haluk.txt")).//file e erisip file satirlari akisa alindi
+                filter(t -> t.contains("basari")).//basari icerme sarti
+                count());//akistaki elemanlar -> satirlar sayildi
+        //TASK 05 --> haluk.txt dosyasindaki farkli kelimeleri  yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki farkli kelimeleri  yazdiriniz. -->  ");
+        System.out.println(Files.
+                lines(Paths.get("src\\day01\\haluk.txt")).//file e erisip file satirlari akisa alindi
+                map(t -> t.split(" ")).
+                flatMap(Arrays::stream).//Arrays classindan stream() ile array
+                distinct().
+                toList());
+        //TASK 06 --> haluk.txt dosyasindaki tum kelimeleri natural order  yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki tum kelimeleri natural order  yazdiriniz. -->  ");
+        //TASK 07 --> haluk.txt dosyasinda "basari" kelimesinin kac kere gectigini buyuk harf kucuk harf bagımsız yaziniz.
+        System.out.println("\n*** haluk.txt dosyasinda basari kelimesinin kac kere gectigini  yazdiriniz. -->  ");
+        //TASK 08 --> haluk.txt dosyasinda "a" harfi gecen kelimelerin sayisini ekrana yazdiran programi yaziniz
+        System.out.println("\n*** haluk.txt dosyasinda a harfi gecen kelimelerin sayisini ekrana yazdiran programi yazdiriniz. -->  ");
+        //TASK 09 --> haluk.txt dosyasinda icinde "a" harfi gecen kelimeleri yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda a harfi gecen kelimeler yazdiriniz. -->  ");
+        //TASK 10 --> haluk.txt dosyasinda kac /farklı harf kullanildigini yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda kac /farklı harf kullanildigini  yazdiriniz. -->  ");
+        //TASK 11 --> haluk.txt dosyasinda kac farkli kelime kullanildigini yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda kac farkli kelime kullanildigini  yazdiriniz. -->  ");
+    }
+}
