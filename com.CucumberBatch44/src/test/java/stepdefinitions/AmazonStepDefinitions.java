df6acfa72cd3b35c420872c1c73f238ca8a948df
Murat Kutlu Tuna <muratkutlutuna@gmail.com>
2022-02-10 20:18:40 +0100

    learning cucumber

--- com.CucumberBatch44/src/test/java/stepdefinitions/AmazonStepDefinitions.java
+++ com.CucumberBatch44/src/test/java/stepdefinitions/AmazonStepDefinitions.java
@@ -2,36 +2,42 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.AmazonPages;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class AmazonStepDefinitions {
 
+    AmazonPages aP = new AmazonPages();
+
     @Given("kullanici amazon sayfasina gider")
     public void kullanici_amazon_sayfasina_gider() {
-        Driver.getDriver().get("https://www.amazon.com");
+        Driver.getDriver().get(ConfigReader.getProperty("AmazonUrl"));
     }
     @Given("iPhone icin arama yapar")
     public void i_phone_icin_arama_yapar() {
-
+        aP.searchBoxElement.sendKeys("iphone"+ Keys.ENTER);
     }
-    @Then("sonuclarin Iphone icerdigini test eder")
+    @Then("sonuclarin iphone icerdigini test eder")
     public void sonuclarin_iphone_icerdigini_test_eder() {
-
+        Assert.assertTrue(aP.resultTextElement.getText().contains("iphone"));
     }
     @Given("tea pot icin arama yapar")
     public void tea_pot_icin_arama_yapar() {
-
+        aP.searchBoxElement.sendKeys("tea pot"+ Keys.ENTER);
     }
     @Then("sonuclarin tea pot icerdigini test eder")
     public void sonuclarin_tea_pot_icerdigini_test_eder() {
-
+        Assert.assertTrue(aP.resultTextElement.getText().contains("tea pot"));
     }
     @Given("flower icin arama yapar")
     public void flower_icin_arama_yapar() {
-
+        aP.searchBoxElement.sendKeys("flower"+ Keys.ENTER);
     }
     @Then("sonuclarin flower icerdigini test eder")
     public void sonuclarin_flower_icerdigini_test_eder() {
-
+        Assert.assertTrue(aP.resultTextElement.getText().contains("flower"));
     }
 }
--- com.CucumberBatch44/src/test/resources/features/US1001_amazonsearch.feature
+++ com.CucumberBatch44/src/test/resources/features/US1001_amazonsearch.feature
@@ -1,15 +1,14 @@
 Feature: US1001_amazon_search
 
   Scenario: TC01_Amazon Search Testi
-
     Given kullanici amazon sayfasina gider
     And iPhone icin arama yapar
-    Then sonuclarin Iphone icerdigini test eder
-
+    Then sonuclarin iphone icerdigini test eder
+  Scenario: TC01_Amazon Search Testi 2
     Given kullanici amazon sayfasina gider
     And tea pot icin arama yapar
     Then sonuclarin tea pot icerdigini test eder
-
+  Scenario: TC01_Amazon Search Testi 3
     Given kullanici amazon sayfasina gider
     And flower icin arama yapar
     Then sonuclarin flower icerdigini test eder
